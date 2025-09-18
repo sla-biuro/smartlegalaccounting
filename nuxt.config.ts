1895d55944c6620415a76165591c37f46e9392cc
Vladdjuga <vladdjuga@gmail.com>
2025-09-18 13:17:08 +0300

    Update Nuxt configuration to set baseURL and cdnURL to root for all environments

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
     preset: 'github_pages'
   },
   app: {
-    baseURL: process.env.NODE_ENV === 'production' ? '/logistics-site/' : '/',
-    cdnURL: process.env.NODE_ENV === 'production' ? '/logistics-site/' : '/'
+    baseURL: '/',
+    cdnURL: '/'
   }
 })
